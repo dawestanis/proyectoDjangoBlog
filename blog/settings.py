bb6c697a5ebae7b93882b567cd7674a305557189
Estanis <dawestanis@gmail.com>
2020-10-17 01:56:01 +0100

    "Terminamos las ultimas modificaciones antes de subirlo a producción."

--- blog/settings.py
+++ blog/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,7 +116,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'estatico')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 """
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'estatico'),
