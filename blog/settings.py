8edf907236009565d7f02db8d71412e432367dbe
Estanis <dawestanis@gmail.com>
2020-10-18 12:03:41 +0100

    Cambio de ddbb

--- blog/settings.py
+++ blog/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.backends.db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
